d2f332ba2263415e7e16226522ade3e3e8451435
Mason <mason.rozumny23@d155.org>
2022-10-09 21:05:21 -0500

    Initial Commit

---
+++ BurgerAppRozumny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerAppRozumny
+//
+//  Created by MASON ROZUMNY on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
